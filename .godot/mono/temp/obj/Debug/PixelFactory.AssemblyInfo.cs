d455558bf1a9f4812ec63bd63dcf1ee32d03132f
Namoris <namoriscz@gmail.com>
2024-05-05 20:34:44 +0200

    Updated 'Iron Ingot' texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b584011129a0e69d66f6b8a2dc86b235280ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2943105436eeb74a17c273965c833cc79e681c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
