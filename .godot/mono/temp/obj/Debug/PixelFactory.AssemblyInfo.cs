f24b7bca17ce96802c5503a2bf7e16a5c47690e8
Namoris <namoriscz@gmail.com>
2024-06-02 18:08:33 +0200

    Updated 'IronOre' drilling particles

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa337c3f7de1e2a01dbce75f838c260fc5f3d431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f41dfbad0f823fb7629a13e3a42afc297f0a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
