0f66fe93e2436911fe22af708345ac7972c1db8f
Namoris <namoriscz@gmail.com>
2024-06-03 08:08:40 +0200

    Updated 'IronPlate' texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f41dfbad0f823fb7629a13e3a42afc297f0a22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24b7bca17ce96802c5503a2bf7e16a5c47690e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
