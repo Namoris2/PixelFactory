deae9bd0e1be83a71592c1e6e77167820f793d49
Namoris <namoriscz@gmail.com>
2024-10-03 10:02:12 +0200

    Added 'Coal' to terrain set

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d45f8e0bace98fcfdcb1d778a335dd6b8ea7b4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbd734769f46faf5b135fc4aede3e00e7817ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
