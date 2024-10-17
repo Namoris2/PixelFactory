5e661db3502eebfdba17dbb769ccc5e615249667
Namoris <namoriscz@gmail.com>
2024-10-17 08:56:16 +0200

    Added 'Wire' and 'SteelIngot' items

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8549021c21f6bda133dff4af59c05d0edacc4870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cebb704e3e0af246ba0e6bdff2a263df7fcd9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
