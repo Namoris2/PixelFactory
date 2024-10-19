f371e5a8a5266e2ac3be5ed13bbed140dbbf9b03
Namoris <namoriscz@gmail.com>
2024-10-19 20:01:02 +0200

    Added 'SteelIngot' recipe

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58c99e239c60e434c2f1b1c26abf79cb31dcc3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8494bd9f95d7509bd347115c3e72cc2c6bfcdd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
