6dcbf297793d7b852225300cb6ad19a374a8e2c4
Namoris <namoriscz@gmail.com>
2024-10-30 14:46:12 +0100

    Fixed unable to unpause game with "Back to game" button

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a5bab9f436da8e392bdb71f9cd0a3c9d9eda1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e6824214a7202e37c63aab555935cb05a53a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
