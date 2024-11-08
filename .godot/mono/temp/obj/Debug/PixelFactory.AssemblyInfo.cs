b793fb57522e7d4beacb8a5315db5c2ec521d143
Namoris <namoriscz@gmail.com>
2024-11-08 18:25:56 +0100

    Updated 'Coal' item texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5629ebf603fba1b9c5454cc1ef2af4c2eaa745a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34582620ce8980a5bddafe02119a118225c5538")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
