868aafc1278aee19057596d9d259959185c72f56
Namoris <namoriscz@gmail.com>
2024-11-11 14:51:04 +0100

    Updated 'SteelBeam' texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04cb92baf852733543265e61dc85e3aa1b296bd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befa05cfbdb464cb6900e7144856c173b4fb0d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
