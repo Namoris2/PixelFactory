734e31b5763f794759bb3e96b7bd6647989f949b
Namoris <namoriscz@gmail.com>
2024-11-19 09:58:48 +0100

    'Coal' generates more frequently

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b07bd58157af45dee848f5ea926bf2df6217b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c92c96c59ab482bae6e54bfc5d286335bd0e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
