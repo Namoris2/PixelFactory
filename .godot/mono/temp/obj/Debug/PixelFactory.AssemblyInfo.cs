e407e112b5fa037cb824c6b3467483f20ce3157a
Namoris <namoriscz@gmail.com>
2024-12-10 10:58:40 +0100

    Added 'Stator' item and recipe

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26df671069822e6bd617b83d796fd9f25861ad28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7baaae616b21f6f131cc8747467f1b504b35e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
