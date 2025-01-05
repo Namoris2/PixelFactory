0db0a4611d6f0ac24fbb9bcd1c34b5bc2ae088a4
Namoris <namoriscz@gmail.com>
2025-01-05 15:57:50 +0100

    Updated 'ModularFrame' texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce13a85562a91dbb1b8aafcc2e6e525c84431917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fc6eaf9622ee004a661da09be94957ab397afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
