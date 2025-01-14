e98649c3bef586bc4a4ced451f9ec3a122406af0
Namoris <namoriscz@gmail.com>
2025-01-14 09:49:22 +0100

    Added collision to 'Assembler'

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b394f277aa1ca36a5f23b6c6e4c9e9f5b2648176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bca68a38740c61b5ca5c79fb9a4fda05ce89d6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
