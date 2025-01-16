70ee42c64c49241f1894fc9831c1367813863abd
Namoris <namoriscz@gmail.com>
2025-01-16 08:51:35 +0100

    Made Build Menu smaller

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e993e6bfa2829e7f33abfc8d6d63b11e4c32d34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51caa1fe55505b2c52468ebf1c0961e065dff36")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
