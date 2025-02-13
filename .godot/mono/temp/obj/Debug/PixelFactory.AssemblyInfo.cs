f0c48eeda746a230db61c8731e414b6b4939b230
Namoris <namoriscz@gmail.com>
2025-02-13 08:41:42 +0100

    Added Research button texture

--- .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PixelFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3b7bf31b04c8b12a1d77e86b640a5b0295ac19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49129277454f164f2cc9e633060fdcb3196beafb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
